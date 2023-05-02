e2089ec77d7d6c6506250cc12597a790b4392684
sylviawangfr <newtyjun@hotmail.com>
2023-05-02 11:50:42 +0800

    delete use_cuda

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,7 +9,7 @@ from transformers import BertTokenizer, BertModel
 
 
 def generate_bert_feature(args):
-    entities = read_entities(args.in_dir)
+    entities = read_entities()
     bert_path = "../saved_models/bert-base-uncased"
     local_models = Path(bert_path)
     if local_models.exists():
@@ -27,19 +27,21 @@ def generate_bert_feature(args):
             pickle.dump(ent2feature, f)
 
 
-def read_entities(in_dir):
+def read_entities():
     entities = []
-    for f in ['test', 'valid', 'train']:
-        df = pd.read_csv(in_dir + f"{f}.txt", sep="\t", header=None)
-        df = pd.concat([df[0], df[1], df[2]]).unique()
-        entities.extend(df.tolist())
-    entities = list(set(entities))
+    in_dir1 = "../dataset_only/CN-82K/"
+    in_dir2 = "../dataset_only/CN-100K/"
+    for in_dir in [in_dir1, in_dir2]:
+        for f in ['test', 'valid', 'train']:
+            df = pd.read_csv(in_dir + f"{f}.txt", sep="\t", header=None)
+            df = pd.concat([df[0], df[1], df[2]]).unique()
+            entities.extend(df.tolist())
+            entities = list(set(entities))
     return entities
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="preprocessing")
-    parser.add_argument('--in_dir', type=str, default="../data/conceptnet-82k/")
     parser.add_argument('--out_dir', type=str, default="../data/saved_entity_embedding/conceptnet/")
     args = parser.parse_args()
     generate_bert_feature(args)
